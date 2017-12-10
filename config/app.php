4b99edfdc5aa943fe0faf3e59b63755ad50a68a1
Jason <jasonwhite1976@yahoo.co.uk>
2017-12-10 14:46:37 +0000

    htaccess - force www

--- config/app.php
+++ config/app.php
@@ -52,8 +52,8 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
-    //'url' => env('APP_URL', 'https://www.hexagonbrain.com'),
+    //'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://www.hexagonbrain.com'),
 
     /*
     |--------------------------------------------------------------------------
