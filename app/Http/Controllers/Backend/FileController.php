0e67385c3f460ed8f7881cc647ccc01dd7b3dd0b
Jason <jasonwhite1976@yahoo.co.uk>
2017-12-22 21:08:56 +0000

    Add summernote

--- app/Http/Controllers/Backend/FileController.php
+++ app/Http/Controllers/Backend/FileController.php
@@ -70,43 +70,31 @@ class FileController extends Controller
         ]);
 
 
-        $message=$request->input('post_content');
+        $data=$request->input('post_content');
 
         $post = new \App\Posts;
 
         $dom = new \DomDocument();
 
-        $dom->loadHtml($message, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+        $dom->loadHtml($data, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
 
         $images = $dom->getElementsByTagName('img');
-       // foreach <img> in the submited message
-        foreach($images as $img){
-            $src = $img->getAttribute('src');
-
-            // if the img source is 'data-url'
-            if(preg_match('/data:image/', $src)){
-                // get the mimetype
-                preg_match('/data:image\/(?<mime>.*?)\;/', $src, $groups);
-                $mimetype = $groups['mime'];
-                // Generating a random filename
-                $filename = uniqid();
-                $filepath = "/uploads/$filename.$mimetype";
-                // @see http://image.intervention.io/api/
-                //$image = Image::make($src)
-                  // resize if required
-                  /* ->resize(300, 200) */
-                  //->encode($mimetype, 100)  // encode file to the specified mimetype
-                  //->save(public_path($filepath));
-                $new_src = asset($filepath);
-                $img->removeAttribute('src');
-                $img->setAttribute('src', $new_src);
-            } // <!--endif
-        } // <!-
+         foreach($images as $k => $img){
+             $data = $img->getAttribute('src');
+             list($type, $data) = explode(';', $data);
+             list(, $data)      = explode(',', $data);
+             $data = base64_decode($data);
+             $image_name= "/uploads/" . time().$k.'.png';
+             $path = public_path() . $image_name;
+             file_put_contents($path, $data);
+             $img->removeAttribute('src');
+             $img->setAttribute('src', $image_name);
+         }
 
         $post->post_content = $dom->saveHTML();
         $post->save();
 
-        return redirect('/admin/post');
+        return redirect('/admin/allposts');
     }
 
     public function delete($id)
