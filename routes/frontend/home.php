f087a3da8eee91f9da91e5b4211b1849974aa746
Jason <jasonwhite1976@yahoo.co.uk>
2018-01-02 21:27:04 +0000

    Force https

--- routes/frontend/home.php
+++ routes/frontend/home.php
@@ -5,8 +5,8 @@
  * All route names are prefixed with 'frontend.'.
  */
 Route::get('/', 'HomeController@index')->name('index');
-Route::get('contact', 'ContactController@index')->name('contact');
-Route::post('contact/send', 'ContactController@send')->name('contact.send');
+Route::get('/contact', 'ContactController@index')->name('contact');
+Route::post('/contact/send', 'ContactController@send')->name('contact.send');
 
 Route::get('links', 'LinksController@index')->name('links');
 
