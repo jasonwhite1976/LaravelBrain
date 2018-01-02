aa7ce9f28f9eedba44f7ae5d63ef8c641c9f769c
Jason <jasonwhite1976@yahoo.co.uk>
2018-01-02 19:42:56 +0000

    Force https

--- config/app.php
+++ config/app.php
@@ -53,7 +53,7 @@ return [
     */
 
     //'url' => env('APP_URL', 'http://localhost'),
-    'url' => env('APP_URL', 'http://www.hexagonbrain.com'),
+    'url' => env('APP_URL', 'https://www.hexagonbrain.com'),
 
     /*
     |--------------------------------------------------------------------------
