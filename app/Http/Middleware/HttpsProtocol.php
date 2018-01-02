495c2260addb94a3d925c4cfb9f39796f0343f50
Jason <jasonwhite1976@yahoo.co.uk>
2018-01-02 19:50:22 +0000

    Force https

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -8,7 +8,7 @@ class HttpsProtocol {
 
     public function handle($request, Closure $next)
     {
-            if (!$request->secure() && env('APP_ENV') === 'production') {
+            if (!$request->secure() && env('APP_ENV') === 'prod') {
                 return redirect()->secure($request->getRequestUri());
             }
 
